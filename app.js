93a5770f2bc0bfe001a6c3d55ecaf25ada4e441c
Arpan Patel <ap.patel1893@gmail.com>
2019-05-03 23:53:31 -0500

    callback chaining

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Houston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if(!process.argv[2]){
+    console.log('provide address')
 
-forecast('-75.7088', 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+} else {
+        geocode(process.argv[2], (error, data) => {
+            if(error) {
+                return console.log(error)
+            }
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if(error) {
+                    return console.log(error)
+                }
+            
+                console.log(data.location)
+                console.log(forecastData)
+            })
+        })
+    }
