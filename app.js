4035e42a9549988434331c5ccf8be069a79207de
Arpan Patel <ap.patel1893@gmail.com>
2019-05-03 23:35:58 -0500

    fix typo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const forecast = require('./utils/forecast')
 geocode('Houston', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
-// })
+})
 
 forecast('-75.7088', 44.1545, (error, data) => {
     console.log('Error', error)
